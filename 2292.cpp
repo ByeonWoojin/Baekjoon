98be7871773ca565d00c7dccf4456bbf7ef03a79
ByeonWoojin <162543327+ByeonWoojin@users.noreply.github.com>
2024-04-09 20:00:47 +0900

    Add files via upload

---
+++ 2292.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int count = 1;
+
+    cin >> a;
+    
+    int add = 6;
+    int max = 1;
+
+    while (1)
+    {
+        if (a <= max) break;    //max 값이 커져서 a보다 커지면 break;
+        
+        max += add;     // 1line => 7, 2line => 8~19 3line =>20~36;    
+        add += 6;       //한 줄당 6의 배수로 커짐, 6,12,18,24
+        ++count;
+    }
+
+    cout << count << endl;
+
+    return 0;
+
+}
---
+++ 2869.cpp
@@ -0,0 +1,39 @@
+﻿/*#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, V;
+    cin >> A >> B >> V;
+
+    int count = 0;
+    int total = 0;
+
+    while (total < V) {
+        count++;
+        total += A; // 낮에 올라간 높이를 더합니다.
+        if (total >= V) break; // 정상에 도달하면 반복문을 종료합니다.
+        total -= B; // 밤에 미끄러진 높이를 뺍니다.
+    }
+
+    cout << count << endl;
+
+    return 0;
+}       ----> 이거는 시간 초과로.... 안됨 제출이
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, V;
+    cin >> A >> B >> V;
+
+    int days = (V - B - 1) / (A - B) + 1;
+
+    cout << days << endl;
+
+    return 0;
+}
+
