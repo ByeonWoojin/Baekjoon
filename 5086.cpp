3f66b9633a468e97b0b39f7fd34ba3fd0b68d586
ByeonWoojin <162543327+ByeonWoojin@users.noreply.github.com>
2024-04-11 00:59:50 +0900

    Add files via upload

---
+++ 5086.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+void result(int first, int second) {
+	if (second % first == 0)
+	{
+		cout << "factor" << endl;
+	}
+	else if (first % second == 0)
+	{
+		cout << "multiple" << endl;
+	}
+	else
+	{
+		cout << "neither" << endl;
+	}
+}
+int main() {
+
+
+	while (true)
+	{
+		int first, second;
+		cin >> first>>second;
+		if (first == 0 && second == 0)
+			break;
+		
+		result(first, second);
+	}
+	return 0;
+}
