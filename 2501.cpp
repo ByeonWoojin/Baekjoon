614954c000dded337d1dbdadbfd45bedb5383e7a
ByeonWoojin <162543327+ByeonWoojin@users.noreply.github.com>
2024-04-12 21:27:04 +0900

    Add files via upload

---
+++ 2501.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, K = 0;
+	cin >> N >> K;
+	int divisor = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		if (N%i == 0)
+		{
+			--K;
+			if (K == 0)
+			{
+				divisor = i;
+			}
+		}
+
+	}
+	cout << divisor << endl;
+
+	return 0;
+}
